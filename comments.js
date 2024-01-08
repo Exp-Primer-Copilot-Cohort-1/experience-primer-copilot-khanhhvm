bdff525601941ee72f2fc34efb6fa1fdf79b3d13
Khanh Vu Minh Hoang <36656542+khanhhvm@users.noreply.github.com>
2024-01-08 22:18:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,20 @@
 // create web server
-// npm install express
-var express = require('express');
-var app = express();
-// npm install body-parser
-var bodyParser = require('body-parser');
-// npm install cors
-var cors = require('cors');
-// npm install mongoose
-var mongoose = require('mongoose');
-// npm install http
-var http = require('http');
-// npm install socket.io
-var socket = require('socket.io');
-// npm install nodemon
-var nodemon = require('nodemon');
-// npm install -g nodemon
-// npm install -g nodemon --save-dev
-// npm install -g nodemon --save
-// npm install -g nodemon --save-optional
-// npm install -g nodemon --save-exact
-// npm install -g nodemon --save-bundle
-// npm install -g nodemon --save-dev --save-exact
-// npm install -g nodemon --save-dev --save-optional
-// npm install -g nodemon --save-dev --save-bundle
-// npm install -g nodemon --save-optional --save-exact
-// npm install -g nodemon --save-optional --save-bundle
-// npm install -g nodemon --save-exact --save-bundle
-// npm install -g nodemon --save-dev --save-optional --save-exact
-// npm install -g nodemon --save-dev --save-optional --save-bundle
-// npm install -g nodemon --save-dev --save-exact --save-bundle
-// npm install -g nodemon --save-optional --save-exact --save-bundle
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev --save-optional
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev --save-optional --save-exact
-// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save
+// 1. import express module
+const express = require('express');
+// 2. create web server object
+const app = express();
+// 3. create router object
+const router = express.Router();
+// 4. register router object
+app.use('/comments', router);
+// 5. add router
+router.get('/show', (req, res) => {
+    res.send('show comments');
+});
+router.get('/add', (req, res) => {
+    res.send('add comments');
+});
+// 6. start web server
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
