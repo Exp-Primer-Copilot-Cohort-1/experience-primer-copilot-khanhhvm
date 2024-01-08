6046bcd987b0b5e00b336573baf099331620fb54
Khanh Vu Minh Hoang <36656542+khanhhvm@users.noreply.github.com>
2024-01-08 22:25:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,22 @@
+// Create web server
+// by using express module
+var express = require('express');
+var app = express();
 // create web server
-// run node comments.js
-// http://
+var server = require('http').createServer(app);
+// create socket io server
+var io = require('socket.io')(server);
+// create mysql connection
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'nodejs'
+});
+// connect to mysql
+connection.connect();
+// set web server port number
+server.listen(3000, function(){
+    console.log('Server Running at http://');
+});
