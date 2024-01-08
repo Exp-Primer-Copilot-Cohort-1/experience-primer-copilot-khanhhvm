cafe90e91e60bfecf1a84395adca52d877ac1493
Khanh Vu Minh Hoang <36656542+khanhhvm@users.noreply.github.com>
2024-01-08 22:15:47 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+// create web server
+// npm install express
+var express = require('express');
+var app = express();
+// npm install body-parser
+var bodyParser = require('body-parser');
+// npm install cors
+var cors = require('cors');
+// npm install mongoose
+var mongoose = require('mongoose');
+// npm install http
+var http = require('http');
+// npm install socket.io
+var socket = require('socket.io');
+// npm install nodemon
+var nodemon = require('nodemon');
+// npm install -g nodemon
+// npm install -g nodemon --save-dev
+// npm install -g nodemon --save
+// npm install -g nodemon --save-optional
+// npm install -g nodemon --save-exact
+// npm install -g nodemon --save-bundle
+// npm install -g nodemon --save-dev --save-exact
+// npm install -g nodemon --save-dev --save-optional
+// npm install -g nodemon --save-dev --save-bundle
+// npm install -g nodemon --save-optional --save-exact
+// npm install -g nodemon --save-optional --save-bundle
+// npm install -g nodemon --save-exact --save-bundle
+// npm install -g nodemon --save-dev --save-optional --save-exact
+// npm install -g nodemon --save-dev --save-optional --save-bundle
+// npm install -g nodemon --save-dev --save-exact --save-bundle
+// npm install -g nodemon --save-optional --save-exact --save-bundle
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev --save-optional
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save --save-dev --save-optional --save-exact
+// npm install -g nodemon --save-dev --save-optional --save-exact --save-bundle --save
